bf571f7b0d0e47a67d49a27ad5abdbee0a9b8cf1
murprite <murprite@gmail.com>
2024-12-17 23:14:28 +0700

    fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,10 +9,4 @@ test('Checking title', async ({ page }) => {
 
 test('get started link', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: '+7XXX-XXX-XX-XX' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
