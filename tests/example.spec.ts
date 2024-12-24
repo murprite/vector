42cb20a82afa4050cee067446763f5f18bd01fc2
murprite <murprite@gmail.com>
2024-12-24 20:51:47 +0700

    fix tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,9 +4,6 @@ import { test, expect } from '@playwright/test';
 test('Checking title', async ({ page }: any) => {
   // TODO: http->https and auto resolve current url (maybe some .env file i dunno)
   await page.goto('http://localhost:3000');
-  await expect(page.title);
-
-  
 });
 
 test('get started link', async ({ page }: any) => {
